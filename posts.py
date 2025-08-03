8921a8d95cd41dd76b7e4d305291b013a8b39b26
Onyekachi Chukwunyere Ebosi <ebosisuccess@gmail.com>
2025-08-03 21:56:20 +0400

    Create posts.py

---
+++ posts.py
@@ -0,0 +1,32 @@
+posts = [
+    {
+        "image_url": "https://i.imgur.com/h5SMLAz.png",
+        "caption": """He never sat there again.
+Not since the day she stopped humming in the kitchen.
+Some absences are louder than noise."""
+    },
+    {
+        "image_url": "https://i.imgur.com/BtLSV15.png",
+        "caption": """I wanted to say sorry.
+But pride held my hand…
+And now the ink has dried without her ever reading it."""
+    },
+    {
+        "image_url": "https://i.imgur.com/PmUv90h.png",
+        "caption": """Every photo had a face.
+But one was missing.
+That was his way of erasing himself."""
+    },
+    {
+        "image_url": "https://i.imgur.com/voYPmcV.png",
+        "caption": """She wished for something simple:
+That time would slow down
+and let her be small a little longer."""
+    },
+    {
+        "image_url": "https://i.imgur.com/RLBm31g.png",
+        "caption": """She left her shoes by the door.
+Said she’d be right back.
+That was six years ago."""
+    }
+]
